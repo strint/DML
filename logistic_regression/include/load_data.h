dba837a5c963f5dcd3ad7bc349d44365edaecce7
xswang <2012wxs@gmail.com>
2016-04-23 14:09:20 +0800

    Update: load_data right

--- logistic_regression/include/load_data.h
+++ logistic_regression/include/load_data.h
@@ -4,16 +4,21 @@
 #include <iostream>
 #include <vector>
 #include "data_struct.h"
+#include "mpi.h"
+
+#define MASTER_ID (0)
+#define FEA_DIM_FLAG (99)
 
 class Load_Data {
 
 public:
     Load_Data();
     ~Load_Data();
-    void load_data(const char *train_file, std::string split_tag);
+    void load_data(const char *train_file, std::string split_tag, int rank, int nproc);
     std::vector<std::vector<sparse_feature> > fea_matrix;
     std::vector<double> label;
-    long int fea_dim;
+    long int fea_dim = 0;
+    long int g_feature_dim = 0;
 
 private:
     void split_line(const std::string& line, const std::string& split_tag, std::vector<std::string>& feature_index);
--- logistic_regression/src/lr_main.cpp
+++ logistic_regression/src/lr_main.cpp
@@ -1,9 +1,6 @@
 #include "lr.h"
 #include "mpi.h"
 #include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include <iomanip>
 #include "gtest/gtest.h"
 #include "config.h"
 
@@ -12,13 +9,14 @@ int main(int argc,char* argv[]){
 
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+    MPI_Comm_size(MPI_COMM_WORLD,&nproc);
     const char *train_data_file = argv[1];
     const char *test_data_file = argv[2];
     std::string split_tag = " ";
-    
-    load_data(train_data_file, split_tag, rank);
-    run(nproc, rank);
+    Load_Data ld; 
+    ld.load_data(train_data_file, split_tag, rank, nproc);
+    //LR lr;
+    //lr.run(nproc, rank);
     MPI::Finalize();
     return 0;
 }
