8fabf40d6b2fa1b95e541987772af63d29aacc19
xswang <2012wxs@gmail.com>
2016-04-23 11:35:11 +0800

    Update: change authority of split_line and get_feature_struct from public to private

--- logistic_regression/include/load_data.h
+++ logistic_regression/include/load_data.h
@@ -6,16 +6,18 @@
 #include "data_struct.h"
 
 class Load_Data {
+
 public:
     Load_Data();
     ~Load_Data();
     void load_data(const char *train_file, std::string split_tag);
-    void split_line(const std::string& line, const std::string& split_tag, std::vector<std::string>& feature_index);
-    void get_feature_struct(std::vector<std::string>& feature_index, std::vector<sparse_feature>& key_val);
-
     std::vector<std::vector<sparse_feature> > fea_matrix;
     std::vector<double> label;
-    sparse_feature sf;
     long int fea_dim;
+
+private:
+    void split_line(const std::string& line, const std::string& split_tag, std::vector<std::string>& feature_index);
+    void get_feature_struct(std::vector<std::string>& feature_index, std::vector<sparse_feature>& key_val);
+    sparse_feature sf;
 };
 #endif
--- logistic_regression/src/lr_main.cpp
+++ logistic_regression/src/lr_main.cpp
@@ -8,22 +8,16 @@
 #include "config.h"
 
 int main(int argc,char* argv[]){  
-    std::cout<<"cmd: ./train -trainfile -testfile"<<std::endl;
-    int rank, nproc, namelen=100;
-    MPI_Request req = {};
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    MPI_Status status;
+    int rank, nproc;
 
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
-    MPI_Get_processor_name(processor_name, &namelen);
-    //exec by main thread    
     const char *train_data_file = argv[1];
-    char *test_data_file = argv[2];
+    const char *test_data_file = argv[2];
     std::string split_tag = " ";
     
-    load_data(train_data_file, split_tag);
+    load_data(train_data_file, split_tag, rank);
     run(nproc, rank);
     MPI::Finalize();
     return 0;
