c1cb42049e24d05475c6c56657fb20cb8c280445
xswang <2012wxs@gmail.com>
2016-04-27 00:03:27 +0800

    update: mpirun

--- mpi_eval/run.sh
+++ mpi_eval/run.sh
@@ -1,1 +1,1 @@
-LD_LIBRARY_PATH=/home/services/xiaoshu/ml/mpi/mpi-peval/log4cpp/lib /home/services/xiaoshu/ml/hpc/bin/mpirun -n 4 mpi_eval ./data/score_file auc_file
+mpirun -n 4 mpi_eval ./data/score_file auc_file
