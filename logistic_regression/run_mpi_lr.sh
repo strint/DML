d7b941f1cad82475f3d4a8167c8c1067c4b15357
xswang <2012wxs@gmail.com>
2016-05-04 20:38:09 +0800

    fixbug: mv train to backup

--- logistic_regression/run_mpi_lr.sh
+++ logistic_regression/run_mpi_lr.sh
@@ -2,8 +2,8 @@
 
 tt=`date`
 mkdir backup/"$tt"
-mv train backup/"$tt"
+#mv train backup/"$tt"
 mv *.log backup/"$tt"
 mv core backup/"$tt"
 #make
-mpirun -np 4 ./train ./data/agaricus.txt.train ./data/agaricus.txt.train
+mpirun -np 4 ./train ./data/agaricus.train ./data/agaricus.test
