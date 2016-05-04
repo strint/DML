d68dba9936289e72a066afe2757b88af5f8e4ac5
xswang <2012wxs@gmail.com>
2016-05-04 20:38:38 +0800

    update: modify read file name for multinode

--- logistic_regression/src/lr_main.cpp
+++ logistic_regression/src/lr_main.cpp
@@ -10,13 +10,15 @@ int main(int argc,char* argv[]){
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&nproc);
-    std::cout<<"my process rank: "<<rank<<", totoal process num: "<<nproc<<std::endl;
+    char train_data_path[1024];
     const char *train_data_file = argv[1];
+    snprintf(train_data_path, 1024, "%s-%05d", train_data_file, rank);
+    char test_data_path[1024];
     const char *test_data_file = argv[2];
+    snprintf(test_data_path, 1024, "%s-%05d", test_data_file, rank);
     std::string split_tag = " ";
     Load_Data ld; 
-    ld.load_data(train_data_file, split_tag, rank, nproc);
-    std::cout<< "process "<< rank <<", local feature dimension: "<<ld.loc_fea_dim<<std::endl;
+    ld.load_data(train_data_path, split_tag, rank, nproc);
     LR lr(&ld, nproc, rank);
     //lr.run();
     MPI::Finalize();
