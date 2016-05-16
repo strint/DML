259e9a44ddda539faf40b7cf5764f2f887530d3a
strint <xiaoyulink@gmail.com>
2016-05-16 12:08:58 +0800

    format

--- logistic_regression/src/owlqn.cpp
+++ logistic_regression/src/owlqn.cpp
@@ -265,9 +265,9 @@ bool LR::meet_criterion(){
 void LR::save_model(){
     if(0 == rank) {
         time_t rawtime;
-        struct tm * timeinfo;
-        char buffer [80];
-        time (&rawtime);
+        struct tm* timeinfo;
+        char buffer[80];
+        time(&rawtime);
         timeinfo = localtime(&rawtime);
         strftime(buffer, 80, "%Y%m%d_%H%M%S", timeinfo);
         std::string time_s = buffer;
