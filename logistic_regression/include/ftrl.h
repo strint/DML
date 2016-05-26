c559da26569445219ff1403956bc235c4a2b8441
xswang <2012wxs@gmail.com>
2016-05-26 10:11:47 +0800

    update: ftrl pipline

--- logistic_regression/include/ftrl.h
+++ logistic_regression/include/ftrl.h
@@ -1,12 +1,34 @@
 #ifndef FTRL_H
 #define FTRL_H
+#include "load_data.h"
 
 class FTRL{
 
 public:
-	FTRL();
+	FTRL(Load_Data* data, int total_num_proc, int my_rank);
 	~FTRL();
+	float* glo_w;
 
+private:
+	Load_Data* data;
+	void init();
+	float sigmoid(float x);
+  	int step;
+	float* loc_w;
+	float* loc_g;
+	float* glo_g;
+
+	float* loc_z;
+	float* loc_sigma;
+	float* loc_n;
+
+	float alpha;
+	float beta;
+	float lambda1;
+	float lambda2;
+
+	int num_proc;
+	int rank;
 };
 
 #endif
--- logistic_regression/src/ftrl.cpp
+++ logistic_regression/src/ftrl.cpp
@@ -1,1 +1,72 @@
 #include "ftrl.h"
+
+FTRL::FTRL(Load_Data* ld, int total_num_proc, int my_rank) 
+    : data(ld), num_proc(total_num_proc), rank(my_rank){
+    init();
+}
+~FTRL::FTRL(){}
+
+void FTRL::init(){
+    loc_w = new float[data->glo_fea_dim]();
+    glo_w = new float[data->glo_fea_dim]();
+    
+    loc_g = new float[data->glo_fea_dim]();
+    glo_g = new float[data->glo_fea_dim]();
+
+    loc_z = new float[data->glo_fea_dim]();
+    loc_sigma = new float[data->glo_fea_dim]();
+    loc_n = new float[data->glo_fea_dim]();
+    
+    alpha = 1.0;
+    beta = 1.0;
+    lambda1 = 1.0;
+    lambda2 = 1.0;
+    
+    step = 0;
+    batch_size = 10;
+}
+
+float FTRL::sigmoid(float x){
+    if(x < -30){
+        return 1e-6;
+    }
+    else if(x > 30){
+        return 1.0;
+    }
+    else{
+        double ex = pow(2.718281828, x);
+        return ex / (1.0 + ex);
+    }
+}
+
+void FTRL::ftrl(){
+    for(int i = 0; i < step; i++){
+	for(int j = i * batch_size; j < (i + 1) * batch_size; j++){
+	    float wx = 0.0;
+	    for(int k = 0; k < data->fea_matrix[j].size(); j++){
+		int index = data->fea_matrix[j][k].idx;
+	        float val = data->fea_matrix[j][k].val;
+		if(abs(loc_z[index]) <= lambda1) loc_w[index] = 0.0;
+		else{
+			float tmpr= 0.0;
+	     		if(loc_z[index] > 0) tmp = loc_z[index] - lambda1;
+			else tmp = loc_z[index] + lambda1;
+			float tmpl = ((beta + sqrt(loc_n[index])) / alpha  + lambda2);
+			loc_w[index] = tmpr / tmpl;
+	    	}
+		wx += loc_w[index] * val;
+	    }
+	    float p = 0.0;
+	    p = sigmoid(wx); 
+
+	    for(int k = 0; k < data->fea_matrix[j].size(); j++){
+		int index = data->fea_matrix[j][k].idx;
+                float val = data->fea_matrix[j][k].val;
+		loc_g[index] = (p - data->label[j]) * val;
+		sigma[index] = (sqrt(loc_n[index] + loc_g[index] * loc_g[index]) - sqrt(loc_n[index])) / alpha;
+		loc_z[index] += loc_g[index] - sigma[index] * loc_w[index];
+		loc_n[index] += loc_g[index] * loc_g[index];
+	    }
+	}
+    }
+}
