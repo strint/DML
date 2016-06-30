3a8ba9607cc0df4f04771f3fb190e6fa8c5faeb2
wangxiaoshu <wangxiaoshu@yidian-inc.com>
2016-06-30 19:07:34 +0800

    update: add fm_ps

---
+++ factorization_machine_ps/lib/ps-lite/build/van.d
@@ -0,0 +1,16 @@
+build/van.o: src/van.cc include/ps/internal/van.h include/ps/base.h \
+ include/ps/internal/utils.h include/dmlc/logging.h include/dmlc/./base.h \
+ include/ps/internal/env.h include/ps/internal/message.h \
+ include/ps/sarray.h include/ps/range.h include/ps/internal/postoffice.h \
+ include/ps/internal/customer.h include/ps/internal/threadsafe_queue.h \
+ src/./network_utils.h src/./meta.pb.h \
+ /data/home/xiaoshu/hadoop_job/ml/DML/factorization_machine_ps/lib/ps-lite/deps/include/google/protobuf/stubs/common.h \
+ /data/home/xiaoshu/hadoop_job/ml/DML/factorization_machine_ps/lib/ps-lite/deps/include/google/protobuf/generated_message_util.h \
+ /data/home/xiaoshu/hadoop_job/ml/DML/factorization_machine_ps/lib/ps-lite/deps/include/google/protobuf/message_lite.h \
+ /data/home/xiaoshu/hadoop_job/ml/DML/factorization_machine_ps/lib/ps-lite/deps/include/google/protobuf/repeated_field.h \
+ /data/home/xiaoshu/hadoop_job/ml/DML/factorization_machine_ps/lib/ps-lite/deps/include/google/protobuf/stubs/type_traits.h \
+ /data/home/xiaoshu/hadoop_job/ml/DML/factorization_machine_ps/lib/ps-lite/deps/include/google/protobuf/stubs/template_util.h \
+ /data/home/xiaoshu/hadoop_job/ml/DML/factorization_machine_ps/lib/ps-lite/deps/include/google/protobuf/extension_set.h \
+ src/./zmq_van.h \
+ /data/home/xiaoshu/hadoop_job/ml/DML/factorization_machine_ps/lib/ps-lite/deps/include/zmq.h \
+ src/./resender.h
